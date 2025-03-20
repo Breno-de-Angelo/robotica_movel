f7a8786c9a9ec84c19c0eacfe1746569a798f6e2
Breno U. de Angelo <brenodeangelo@gmail.com>
2025-03-19 23:27:14 -0300

    Criacao ROS ws

---
+++ src/controlador_quadrimotor/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'controlador_quadrimotor'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='breno',
+    maintainer_email='brenodeangelo@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'seguimento_trajetoria = controlador_quadrimotor.seguimento_trajetoria:main'
+        ],
+    },
+)
